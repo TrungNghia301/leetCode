c5f47e95414433db5272649ac1118fe306c48c86
TrungNghia301 <nghiatrung.swd.301@gmail.com>
2024-08-05 21:37:31 +0700

    make new table to Select (subquery

---
+++ sql_50/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,10 @@
+Select Q.person_name
+from
+(
+    Select person_name, SUM(weight) OVER (ORDER BY turn) AS total_weight ,turn
+    From Queue
+)  
+As Q
+Where Q.total_weight <= 1000
+Order by Q.turn Desc
+Limit 1
