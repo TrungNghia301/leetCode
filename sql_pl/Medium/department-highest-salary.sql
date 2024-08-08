2bfc19427a0c016f94e5fbdea7b760093590be7f
TrungNghia301 <nghiatrung.swd.301@gmail.com>
2024-08-08 21:20:45 +0700

    Dense_rank() - Subquery

---
+++ sql_pl/Medium/department-highest-salary.sql
@@ -0,0 +1,12 @@
+Select d.name as Department, rt.name as Employee, rt.salary as Salary
+From(
+WITH RankedValues AS (
+    SELECT *,
+           DENSE_RANK() OVER (PARTITION BY departmentId  ORDER BY salary DESC) AS RankNum
+    FROM Employee
+)
+SELECT *
+FROM RankedValues
+WHERE RankNum <= 1
+) as rt 
+Left join Department d on d.id =  rt.departmentId 
